102484c7a40ae05585e6a69707d5030580997b30
louis <louiscklaw@gmail.com>
2017-12-16 13:07:35 +0800

    update on unregister -> register,

--- fabfile.py
+++ fabfile.py
@@ -8,9 +8,11 @@ import os
 # import sys
 
 from datetime import datetime
-from fabric.api import run, cd, env, task, local, prefix,settings
+from fabric.api import run, cd, env, task, local, prefix,settings, quiet
 from fabric.colors import *
 import re
+
+import time
 
 env.hosts = ['192.168.88.6']
 
@@ -29,6 +31,7 @@ PATH_NOT_FOR_VERSIONING.append(
 
 EPOCH_DATETIME = datetime.now().strftime('%s')
 
+
 env.use_ssh_config = True
 
 
@@ -40,6 +43,21 @@ LOCAL_DIR, REMOTE_DIR = (
 
 GITLAB_SERVICE_NAME = 'gitlab'
 
+
+
+
+GITLAB_NAME = 'gitlab'
+GITLAB_IMAGE_NAME = 'gitlab/gitlab-ce'
+
+GITLAB_RUNNER_NAME = 'gitlabrunner'
+GITLAB_SHELL_RUNNER_NAME = 'gitlab_shell_runner'
+
+GITLAB_RUNNER_CONTAINER = os.path.sep.join([
+    REMOTE_DIR, GITLAB_RUNNER_NAME
+])
+
+
+
 @task
 def sync_files():
     """to sync file between develop machine and the docker host"""
@@ -47,7 +65,7 @@ def sync_files():
         'rsync -avrPz --exclude .git --exclude .vscode %s/ logic@192.168.88.6:%s' % (LOCAL_DIR, REMOTE_DIR))
 
 def gitrunner_register():
-    run('docker-compose exec gitlab_shell_runner gitlab-runner')
+    run('docker-compose exec gitlab_shell_runner gitlab-ci-multi-runner')
 
 def parse_gitrunner_list(list_input):
     pattern=r'(?P<m_reg_name>.+?)\s+Executor=(?P<m_executor>.+?) Token=(?P<m_token>.{0,30}) URL=(?P<m_url>.+)'
@@ -57,7 +75,7 @@ def parse_gitrunner_list(list_input):
 
 def gitrunner_list_unregister_all(container_name):
 
-    registration_list = run('docker-compose exec %s gitlab-runner list' % container_name)
+    registration_list = run('docker-compose exec %s gitlab-ci-multi-runner list' % container_name)
 
     # remove ansi color cod
     registration_list = registration_list.replace('\x1b[0;m','')
@@ -65,57 +83,35 @@ def gitrunner_list_unregister_all(container_name):
     parsed_list = parse_gitrunner_list(registration_list)
     for m in parsed_list:
         (reg_name, executor, token, url) = m
-        # gitlab-runner unregister -t ab13234665cd17f538efe718a6a177 -u https://repo.louislabs.com/
+        # gitlab-ci-multi-runner unregister -t ab13234665cd17f538efe718a6a177 -u https://repo.louislabs.com/
         with settings(warn_only=True):
-            run('docker-compose exec %s gitlab-runner unregister -t %s  -u %s ' % (container_name, token, url))
+            run('docker-compose exec %s gitlab-ci-multi-runner unregister -t %s  -u %s ' % (container_name, token, url))
 
 def rebuild_gitlab(gitlab_container_name):
     print(green('building gitlab', True))
-    with cd(REMOTE_DIR):
+    with cd(REMOTE_DIR), quiet():
         run('docker-compose up -d --remove-orphans %s' % gitlab_container_name)
         # run('docker-compose exec %s gitlab-rake gitlab:env:info' % gitlab_container_name)
         # run('docker-compose exec %s gitlab-rake gitlab:check SANITIZE=true' % gitlab_container_name)
 
+def register_gitlab_runner(container_name):
+    with settings(warn_only=True):
+        run('docker-compose exec %s gitlab-ci-multi-runner register --non-interactive  --executor shell --name shell_runner --url https://repo.louislabs.com/ --registration-token $REG_TOKEN' % container_name)
+
 def rebuild_gitlab_runner(container_name):
     print(green('building gitlab runner', True))
-    with cd(REMOTE_DIR), prefix('source .env'):
+    with cd(REMOTE_DIR), prefix('source .env'), quiet():
         run('docker-compose up -d --remove-orphans %s' % container_name)
 
-def print_gitlab_runner(container_name):
-        with settings(warn_only=True):
-            run('docker-compose exec %s gitlab-runner list' % container_name)
-            run('docker-compose exec %s gitlab-runner verify --delete' % container_name)
-            run('docker-compose exec %s gitlab-runner unregister --all-runners' % container_name)
-            run('docker-compose exec %s gitlab-runner list' % container_name)
+    unregister_gitlab_runner(container_name)
+    register_gitlab_runner(container_name)
 
-            run('docker-compose exec %s gitlab-runner register --non-interactive --name shell_runner --url https://repo.louislabs.com/ --registration-token $REG_TOKEN --executor shell' % container_name)
-
-@task
-def rebuild_container():
-
-    sync_files()
-    GITLAB_NAME = 'gitlab'
-    GITLAB_IMAGE_NAME = 'gitlab/gitlab-ce'
-
-    GITLAB_RUNNER_NAME = 'gitlabrunner'
-    GITLAB_SHELL_RUNNER_NAME = 'gitlab_shell_runner'
-
-    GITLAB_RUNNER_CONTAINER = os.path.sep.join([
-        REMOTE_DIR, GITLAB_RUNNER_NAME
-    ])
-
-    # print(green('rebuild gitlabrunner base image'))
-    # run('docker build -t gitlabrunner:latest %s' % GITLAB_RUNNER_CONTAINER )
-
-    with cd(REMOTE_DIR):
-        run('docker-compose build')
-        run('docker-compose down  --remove-orphans')
-        rebuild_gitlab(GITLAB_NAME)
-        rebuild_gitlab_runner(GITLAB_SHELL_RUNNER_NAME)
-
-        gitlab_check(GITLAB_SERVICE_NAME)
-        run('docker-compose ps')
-
+def unregister_gitlab_runner(container_name):
+    with settings(warn_only=True):
+        run('docker-compose exec %s gitlab-ci-multi-runner list' % container_name)
+        run('docker-compose exec %s gitlab-ci-multi-runner verify --delete' % container_name)
+        run('docker-compose exec %s gitlab-ci-multi-runner unregister --all-runners' % container_name)
+        run('docker-compose exec %s gitlab-ci-multi-runner list' % container_name)
 
 def gitlab_check(container_name):
     with cd(REMOTE_DIR):
@@ -130,4 +126,38 @@ def reload_config():
         run('docker-compose exec %s /bin/bash gitlab-ctl reconfigure' % GITLAB_SERVICE_NAME)
         gitlab_check(GITLAB_SERVICE_NAME)
         # run('docker exec -it gitlab update-permissions')
+        run('docker-compose ps')
+
+@task
+def rebuild_container():
+
+    sync_files()
+
+    # print(green('rebuild gitlabrunner base image'))
+    # run('docker build -t gitlabrunner:latest %s' % GITLAB_RUNNER_CONTAINER )
+    print(green('building gitlabrunner image'))
+    with cd(GITLAB_RUNNER_CONTAINER), quiet():
+        run('docker build -t gitlabrunner .')
+
+    # for the gitrunner token
+    with cd(REMOTE_DIR), prefix('source .env'):
+        print(green('putting all the things down'))
+        run('docker-compose build')
+        run('docker-compose down  --remove-orphans')
+
+        rebuild_gitlab(GITLAB_NAME)
+
+        # NOTE: the gitlab may not be wake up in time . so some sleep is required
+        wait_for_gitlab_wakeup=60
+        print(yellow('wait a moment for gitlab up...%d seconds' % wait_for_gitlab_wakeup))
+        time.sleep(wait_for_gitlab_wakeup)
+        gitlab_check(GITLAB_SERVICE_NAME)
+
+@task
+def rebuild_runner():
+    with cd(REMOTE_DIR), prefix('source .env'):
+        print(green('start runner building'))
+        rebuild_gitlab_runner(GITLAB_SHELL_RUNNER_NAME)
+
+        print(green('building done'))
         run('docker-compose ps')
